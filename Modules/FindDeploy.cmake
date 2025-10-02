b825fdcb139a4286fef55846ed99f992da23c915
Scott Aron Bloom <scott@towel42.com>
2025-10-02 02:22:20 +0100

    Deploy now has optional extra targets to deploy against

--- Modules/FindDeploy.cmake
+++ Modules/FindDeploy.cmake
@@ -29,33 +29,27 @@ if( NOT DEFINED USE_QT OR USE_QT )
 	if( NOT DEFINED DEPLOYQT_EXECUTABLE )
 		# Retrieve the absolute path to qmake and then use that path to find
 		# the <os>deployqt binaries
-		get_target_property(_qmake_executable Qt6::qmake IMPORTED_LOCATION)
-		get_filename_component(_qt_bin_dir "${_qmake_executable}" DIRECTORY)
-
-		if( WIN32 )
-			find_program(DEPLOYQT_EXECUTABLE windeployqt HINTS "${_qt_bin_dir}")
-			if(NOT DEPLOYQT_EXECUTABLE)
-				message(FATAL_ERROR "windeployqt not found")
-			endif()
-			message(STATUS "Found windeployqt: ${DEPLOYQT_EXECUTABLE}")
-            # Doing this with MSVC 2015 requires CMake 3.6+
-            if( (MSVC_VERSION VERSION_EQUAL 1900 OR MSVC_VERSION VERSION_GREATER 1900) AND CMAKE_VERSION VERSION_LESS "3.6")
-                message(WARNING "Deploying with MSVC 2015+ requires CMake 3.6+")
+		find_program(_qtpaths_exec qtpaths REQUIRED)
+		#message( STATUS "Found qtpaths=${_qtpaths_exec}")
+		execute_process( 
+			COMMAND ${_qtpaths_exec} -qt-query QT_INSTALL_BINS
+			OUTPUT_VARIABLE _qt_bin_dir
+			OUTPUT_STRIP_TRAILING_WHITESPACE
+		)
+		#message( STATUS "bindir = ${_qt_bin_dir}")
+		if ( NOT EXISTS "${_qt_bin_dir}" )
+			MESSAGE( FATAL "Could not find Qt's Bin Dir'")
 		endif()
-        ELSEIF( APPLE )
-            find_program(DEPLOYQT_EXECUTABLE macdeployqt HINTS "${_qt_bin_dir}")
-            if(NOT DEPLOYQT_EXECUTABLE)
-                message(FATAL_ERROR "macdeployqt not found")
-            endif()
-            message(STATUS "Found macdeployqt: ${DEPLOYQT_EXECUTABLE}")
-        ELSEIF( UNIX )
-            find_program(DEPLOYQT_EXECUTABLE linuxdeployqt HINTS "${_qt_bin_dir}")
-            if(NOT DEPLOYQT_EXECUTABLE)
-                message(WARNING "linuxdeployqt not found")
-            else()
-                message(STATUS "Found macdeployqt: ${DEPLOYQT_EXECUTABLE}")
-            endif()
-        ENDIF()
+
+		find_program(DEPLOYQT_EXECUTABLE windeployqt HINTS "${_qt_bin_dir}")
+		if(NOT DEPLOYQT_EXECUTABLE)
+			message(FATAL_ERROR "windeployqt not found")
+		endif()
+		message(STATUS "Found windeployqt: ${DEPLOYQT_EXECUTABLE}")
+        # Doing this with MSVC 2015 requires CMake 3.6+
+        if( (MSVC_VERSION VERSION_EQUAL 1900 OR MSVC_VERSION VERSION_GREATER 1900) AND CMAKE_VERSION VERSION_LESS "3.6")
+            message(WARNING "Deploying with MSVC 2015+ requires CMake 3.6+")
+		endif()
 	mark_as_advanced(DEPLOYQT_EXECUTABLE)
 endif()
 
@@ -72,33 +66,44 @@ endif()
 		endif()
 
 		set( options )
-		set( oneValueArgs INSTALL_ONLY NON_INSTALL_ONLY NO_TRANSLATIONS )
-		set( multiValueArgs )
+		set( oneValueArgs INSTALL_ONLY NON_INSTALL_ONLY NO_TRANSLATIONS  )
+		set( multiValueArgs EXTRA_TARGETS )
 
 		cmake_parse_arguments( "" "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN} )
 		if ( _NO_TRANSLATIONS )
 			SET( NO_TRANSLATIONS_OPT "--no-translations")
 		endif()
 
+		if ( _EXTRA_TARGETS )
+			foreach(currExtraTarget ${_EXTRA_TARGETS})
+				SET( EXTRA_TARGETS_OPT ${EXTRA_TARGETS_OPT} "$<TARGET_FILE:${currExtraTarget}>")
+
+				set( targetLibs 
+					$<TARGET_FILE:${currExtraTarget}> 
+					$<TARGET_PDB_FILE:${currExtraTarget}>
+				) 
+
+				foreach(lib ${targetLibs})
+					get_filename_component(filename "${lib}" NAME)
+					add_custom_command(TARGET ${target} POST_BUILD
+						COMMAND "${CMAKE_COMMAND}" -E echo "Deploying Target Library Library '${filename}' for '${target}'"
+						COMMAND "${CMAKE_COMMAND}" -E copy_if_different "${lib}" \"$<TARGET_FILE_DIR:${target}>\"
+					)
+				endforeach()
+			endforeach()
+		endif()
+
 		if ( NOT _INSTALL_ONLY )
-			SET(_QTDEPLOY_TARGET_DIR "$<TARGET_FILE:${target}>" )
-			IF( WIN32 )
-				SET(_QTDEPLOY_OPTIONS_LCL "--verbose=1;--no-compiler-runtime;--no-opengl-sw;--no-system-dxc-compiler;--pdb;${NO_TRANSLATIONS_OPT}" )
-			ELSEIF( APPLE )
-				SET(_QTDEPLOY_TARGET_DIR "$<TARGET_FILE:${target}>/../.." )
-				SET(_QTDEPLOY_OPTIONS_LCL "--verbose=1;--no-compiler-runtime;--always-overwrite;${NO_TRANSLATIONS_OPT}" )
-			ELSEIF( UNIX )
-				SET(_QTDEPLOY_OPTIONS_LCL "-verbose=1;${NO_TRANSLATIONS_OPT}" )
-				return()
-			ENDIF()
+			SET(_QTDEPLOY_TARGET "$<TARGET_FILE:${target}>" )
+			SET(_QTDEPLOY_OPTIONS_LCL "--dir=\"$<TARGET_FILE_DIR:${target}>\";--verbose=1;--no-compiler-runtime;--no-opengl-sw;--no-system-dxc-compiler;--pdb;${NO_TRANSLATIONS_OPT}" )
 
 			# Run deployqt immediately after build to make the build area "complete"
 			add_custom_command(TARGET ${target} POST_BUILD
-				COMMAND "${CMAKE_COMMAND}" -E echo "Deploying Qt to Build Area for Project '${target}' using '${DEPLOYQT_EXECUTABLE}' ..."
+				COMMAND "${CMAKE_COMMAND}" -E echo "Deploying Qt to Build Area for Project '${target}' using '${DEPLOYQT_EXECUTABLE}' ${_QTDEPLOY_OPTIONS_LCL} ${_QTDEPLOY_TARGET} ${EXTRA_TARGETS_OPT}"
 				COMMAND "${CMAKE_COMMAND}" -E
 					env PATH="${_qt_bin_dir}" "${DEPLOYQT_EXECUTABLE}"
 						${_QTDEPLOY_OPTIONS_LCL}
-						${_QTDEPLOY_TARGET_DIR}
+						${_QTDEPLOY_TARGET} ${EXTRA_TARGETS_OPT}
 			)
 		endif()
 		
@@ -115,13 +120,7 @@ endif()
 			install(CODE
 				"
 				file(READ \"${CMAKE_CURRENT_BINARY_DIR}/${target}_\${CMAKE_INSTALL_CONFIG_NAME}_path\" _file)
-				IF( WIN32 )
-					SET(_QTDEPLOY_OPTIONS \"--dry-run;--list;mapping;--no-compiler-runtime;--no-opengl-sw;--no-system-dxc-compiler;${NO_TRANSLATIONS_OPT}\" )
-				ELSEIF( APPLE )
-					SET(_QTDEPLOY_OPTIONS \"--dry-run;--list;mapping;${NO_TRANSLATIONS_OPT}\" )
-				ELSEIF( UNIX )
-					SET(_QTDEPLOY_OPTIONS \"--dry-run;--list;mapping;${NO_TRANSLATIONS_OPT}\" )
-				ENDIF()
+				SET(_QTDEPLOY_OPTIONS \"--dry-run;--list;mapping;--no-compiler-runtime;--no-opengl-sw;--no-system-dxc-compiler;${NO_TRANSLATIONS_OPT}\" )
 
 				MESSAGE( STATUS \"Deploying Qt to the Install Area '\${CMAKE_INSTALL_PREFIX}/${directory}' for Project '${target}' using '${DEPLOYQT_EXECUTABLE}' ...\" )
 				execute_process(
