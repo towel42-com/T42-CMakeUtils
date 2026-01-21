405e7fdf8a15684e409b2b2f62faf2cc07bee91c
Scott Aron Bloom <scott@towel42.com>
2026-01-21 02:41:41 +0000

    Created Utilitity cmake package

--- Modules/FindT42Utils.cmake
+++ Modules/FindT42Utils.cmake
@@ -14,3 +14,30 @@ function(PrintAllVariables)
     endforeach()
     message(STATUS "--- End of CMake Variables Dump ---")
 endfunction()
+
+
+## https://stackoverflow.com/questions/32183975/how-to-print-all-the-properties-of-a-target-in-cmake/56738858#56738858
+## https://stackoverflow.com/a/56738858/3743145
+
+## Get all properties that cmake supports
+function(print_target_properties tgt)
+    execute_process(COMMAND cmake --help-property-list OUTPUT_VARIABLE __CMAKE_PROPERTY_LIST)
+    ## Convert command output into a CMake list
+    STRING(REGEX REPLACE ";" "\\\\;" __CMAKE_PROPERTY_LIST "${__CMAKE_PROPERTY_LIST}")
+    STRING(REGEX REPLACE "\n" ";" __CMAKE_PROPERTY_LIST "${__CMAKE_PROPERTY_LIST}")
+
+    list(REMOVE_DUPLICATES __CMAKE_PROPERTY_LIST)
+
+    if(NOT TARGET ${tgt})
+      message("There is no target named '${tgt}'")
+      return()
+    endif()
+
+    foreach (prop ${__CMAKE_PROPERTY_LIST})
+        string(REPLACE "<CONFIG>" "${CMAKE_BUILD_TYPE}" prop ${prop})
+        get_target_property(propval ${tgt} ${prop})
+        if (propval)
+            message ("${tgt} ${prop} = ${propval}")
+        endif()
+    endforeach(prop)
+endfunction()
