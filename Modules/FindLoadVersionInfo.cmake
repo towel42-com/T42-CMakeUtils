dbb1d5d07c30f3f0f9d1f39c8f3c982dbc996458
Scott Aron Bloom <scott@towel42.com>
2026-03-12 19:17:07 +0100

    Always checks to see if file needs updating

--- Modules/FindLoadVersionInfo.cmake
+++ Modules/FindLoadVersionInfo.cmake
@@ -1,26 +1,29 @@
+find_package(InstallFile REQUIRED)
 
 FUNCTION(CreateVersionInfoFile)
     if ( NOT APP_NAME )
         MESSAGE( FATAL_ERROR "APP_NAME must be set" )
+    endif()
+    
+    if ( NOT MAIN_EXE_PROJECT_NAME )
+        MESSAGE( FATAL_ERROR "MAIN_EXE_PROJECT_NAME must be set" )
     endif()
 
     if ( NOT VERSIONINFO_FILE )
         MESSAGE( FATAL_ERROR "VERSIONINFO_FILE must be set" )
     endif()
 
-    set( TEMPLATE_FILE ${CMAKE_SOURCE_DIR}/T42-CMakeUtils/Modules/VersionInfo.cmake.in )
-
-    if ( NOT START_YEAR )
-        STRING(TIMESTAMP START_YEAR "%Y" UTC)
+    if ( "${MAJOR_VERSION}" STREQUAL "" )
+        MESSAGE( FATAL_ERROR "MAJOR_VERSION must be set" )
     endif()
 
-    if ( NOT MAJOR_VERSION )
-        SET( MAJOR_VERSION 0)
+    if ( "${MINOR_VERSION}" STREQUAL "" )
+        MESSAGE( FATAL_ERROR "MINOR_VERSION must be set" )
     endif()
 
-    if ( NOT MINOR_VERSION )
-        SET( MINOR_VERSION 1)
-    endif()
+    SET( TMP_FILE ${VERSIONINFO_FILE}.bak )
+    SET( OUTFILE ${VERSIONINFO_FILE} )
+    set( TEMPLATE_FILE ${CMAKE_CURRENT_FUNCTION_LIST_DIR}/VersionInfo.cmake.in )
 
     if ( NOT VENDOR )
         SET( VENDOR "Towel 42 Development, LLC" )
@@ -31,46 +34,39 @@ FUNCTION(CreateVersionInfoFile)
     endif()
 
     if ( NOT PRODUCT_HOMEPAGE )
-        get_filename_component(PROJ_NAME ${CMAKE_CURRENT_SOURCE_DIR} NAME )
-        SET( PRODUCT_HOMEPAGE "https://github.com/towel42-com/${PROJ_NAME}" )
+        SET( PRODUCT_HOMEPAGE "https://github.com/towel42-com/${MAIN_EXE_PROJECT_NAME}" )
     endif()
 
     if ( NOT EMAIL )
         SET( EMAIL "support@towel42.com" )
     endif()
 
-    MESSAGE( STATUS "T42-CMakeUtils:         APP_NAME- ${APP_NAME}" )
-    MESSAGE( STATUS "T42-CMakeUtils:       START_YEAR- ${START_YEAR}" )
-    MESSAGE( STATUS "T42-CMakeUtils:    MAJOR_VERSION- ${MAJOR_VERSION}" )
-    MESSAGE( STATUS "T42-CMakeUtils:    MINOR_VERSION- ${MINOR_VERSION}" )
-    MESSAGE( STATUS "T42-CMakeUtils:           VENDOR- ${VENDOR}" )
-    MESSAGE( STATUS "T42-CMakeUtils:         HOMEPAGE- ${HOMEPAGE}" )
-    MESSAGE( STATUS "T42-CMakeUtils: PRODUCT_HOMEPAGE- ${PRODUCT_HOMEPAGE}" )
-    MESSAGE( STATUS "T42-CMakeUtils:            EMAIL- ${EMAIL}" )
-    MESSAGE( STATUS "T42-CMakeUtils:            EMAIL- ${EMAIL}" )
-    MESSAGE( STATUS "T42-CMakeUtils:    TEMPLATE_FILE- ${TEMPLATE_FILE}" )
-    MESSAGE( STATUS "T42-CMakeUtils: VERSIONINFO_FILE- ${VERSIONINFO_FILE}" )
-    
+    #MESSAGE( STATUS "T42-CMakeUtils:         APP_NAME- ${APP_NAME}" )
+    #MESSAGE( STATUS "T42-CMakeUtils:    MAJOR_VERSION- ${MAJOR_VERSION}" )
+    #MESSAGE( STATUS "T42-CMakeUtils:    MINOR_VERSION- ${MINOR_VERSION}" )
+    #MESSAGE( STATUS "T42-CMakeUtils:           VENDOR- ${VENDOR}" )
+    #MESSAGE( STATUS "T42-CMakeUtils:         HOMEPAGE- ${HOMEPAGE}" )
+    #MESSAGE( STATUS "T42-CMakeUtils: PRODUCT_HOMEPAGE- ${PRODUCT_HOMEPAGE}" )
+    #MESSAGE( STATUS "T42-CMakeUtils:            EMAIL- ${EMAIL}" )
+    #MESSAGE( STATUS "T42-CMakeUtils:    TEMPLATE_FILE- ${TEMPLATE_FILE}" )
+    #MESSAGE( STATUS "T42-CMakeUtils: VERSIONINFO_FILE- ${VERSIONINFO_FILE}" )
+    message( CHECK_START "Generating/Updating VersionInfo.cmake" )
+
     configure_file( 
         ${TEMPLATE_FILE}
-        ${VERSIONINFO_FILE}
+        ${TMP_FILE}
         @ONLY
         NEWLINE_STYLE WIN32
     )
+
+    InstallFile( ${TMP_FILE} ${OUTFILE} REMOVE_ORIG ) # creates a dependency on TMP_OUTFILE
+    message( CHECK_PASS "Generated/Updated" )
 endfunction()
 
 function(LoadVersionInfoFile)
-    set( VERSIONINFO_FILE ${CMAKE_SOURCE_DIR}/VersionInfo.cmake)
+    set( VERSIONINFO_FILE ${CMAKE_BINARY_DIR}/VersionInfo.cmake)
 
-    if ( NOT EXISTS "${VERSIONINFO_FILE}" )
-        message( STATUS "${VERSIONINFO_FILE} NOT exist, generating" )
-        
-        CreateVersionInfoFile()
-
-        if ( NOT EXISTS "${VERSIONINFO_FILE}" )
-            message( FATAL_ERROR "Error generating ${VERSIONINFO_FILE}" )
-        endif()
-    endif()
+    CreateVersionInfoFile()
 
     include( ${VERSIONINFO_FILE} OPTIONAL RESULT_VARIABLE VAR)
     
