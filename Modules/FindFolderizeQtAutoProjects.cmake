c31930b50c7d28b67e9a6a47ba603cceff01d650
Scott Aron Bloom <scott@towel42.com>
2026-04-15 19:12:02 +0100

    Folderize Qt no longer necessary

--- Modules/FindFolderizeQtAutoProjects.cmake
+++ Modules/FindFolderizeQtAutoProjects.cmake
@@ -20,45 +20,52 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-cmake_minimum_required(VERSION 3.31)
-find_package( FindAllDirectories REQUIRED )
-
-function( FolderizeQtAutoProjects )
-    set( options "")
-    set( oneValueArgs FOLDER_NAME TOPDIR )
-    set( multiValueArgs )
-
-    cmake_parse_arguments(PARSE_ARGV 0 arg
-        "${options}" "${oneValueArgs}" "${multiValueArgs}"
-    )
-
-    if ( NOT arg_TOPDIR )
-        SET( arg_TOPDIR ${CMAKE_SOURCE_DIR} )
-    endif()
-
-    if ( NOT arg_FOLDER_NAME )
-        SET( arg_FOLDER_NAME QtAutoProjects )
-    endif()
+message( FATAL_ERROR "Do not use, instead add
+    find_package(Qt6 REQUIRED)
+    SET_PROPERTY(GLOBAL PROPERTY QT_TARGETS_FOLDER CMakePredefinedTargets/QtInternalTargets)
+    qt_standard_project_setup()
     
-    message( STATUS "Searching for Qt Auto Projects in=${arg_TOPDIR}" )
-    FindAllDirectories(${arg_TOPDIR} _ALLDIRS )
-    #message( STATUS "BUILDSYSTEM_TARGETS=${BUILDSYSTEM_TARGETS}" )
- 
-    foreach( dir ${_ALLDIRS} )
-        #MESSAGE( STATUS "dir=${dir}" )
-        get_property(targets DIRECTORY ${dir} PROPERTY "BUILDSYSTEM_TARGETS")
-        #MESSAGE( STATUS "targets=${targets}" )
-        set( _suffixes _qmlimportscan _other_files autogen _automoc_json_extraction qt_internal_plugins)
-        STRING( JOIN ")|(" _regex ${_suffixes} )
-        STRING( CONCAT _regex "^.*(" ${_regex} ")$" )
-        foreach( _target ${targets} )
-            get_target_property(target_type ${_target} TYPE)
-            #MESSAGE( STATUS "${dir} - ${_target} - TARGETTYPE=${target_type}" )
-            if( ${_target} MATCHES ${_regex} )
-                message( STATUS "Adding target '${_target}' to ${arg_FOLDER_NAME}" )
-                set_target_properties( ${_target} PROPERTIES FOLDER ${arg_FOLDER_NAME} )
-            endif()
-        endforeach()
-    endforeach()
-endfunction()
+    To your primary CMakeLists.txt after the find_package( Qt ) call" )
+#cmake_minimum_required(VERSION 3.31)
+#find_package( FindAllDirectories REQUIRED )
 
+#function( FolderizeQtAutoProjects )
+#    set( options "")
+#    set( oneValueArgs FOLDER_NAME TOPDIR )
+#    set( multiValueArgs )
+#
+#    cmake_parse_arguments(PARSE_ARGV 0 arg
+#        "${options}" "${oneValueArgs}" "${multiValueArgs}"
+#    )
+#
+#    if ( NOT arg_TOPDIR )
+#        SET( arg_TOPDIR ${CMAKE_SOURCE_DIR} )
+#    endif()
+#
+#    if ( NOT arg_FOLDER_NAME )
+#        SET( arg_FOLDER_NAME QtAutoProjects )
+#    endif()
+#    
+#    message( STATUS "Searching for Qt Auto Projects in=${arg_TOPDIR}" )
+#    FindAllDirectories(${arg_TOPDIR} _ALLDIRS )
+#    #message( STATUS "BUILDSYSTEM_TARGETS=${BUILDSYSTEM_TARGETS}" )
+# 
+#    foreach( dir ${_ALLDIRS} )
+#        #MESSAGE( STATUS "dir=${dir}" )
+#        get_property(targets DIRECTORY ${dir} PROPERTY "BUILDSYSTEM_TARGETS")
+#        #MESSAGE( STATUS "targets=${targets}" )
+#        set( _suffixes _qmlimportscan _other_files autogen _automoc_json_extraction qt_internal_plugins)
+#        STRING( JOIN ")|(" _regex ${_suffixes} )
+#        STRING( CONCAT _regex "#.*(" ${_regex} ")$" )
+#        foreach( _target ${targets} )
+#            get_target_property(target_type ${_target} TYPE)
+#            #MESSAGE( STATUS "${dir} - ${_target} - TARGETTYPE=${target_type}" )
+#            if( ${_target} MATCHES ${_regex} )
+#                message( STATUS "Adding target '${_target}' to ${arg_FOLDER_NAME}" )
+#                set_target_properties( ${_target} PROPERTIES FOLDER ${arg_FOLDER_NAME} )
+#            endif()
+#        endforeach()
+#    endforeach()
+#endfunction()
+#
+#
