44cc913f38781efd38b115a9b1c7ba0a4125ee2f
Scott Aron Bloom <scott@towel42.com>
2026-04-16 01:03:23 +0100

    Use json for visual studio verification

--- Modules/FindVerifyVisualStudio.cmake
+++ Modules/FindVerifyVisualStudio.cmake
@@ -19,6 +19,25 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
+
+FUNCTION( getInstallMajorVersion versionString VARNAME)
+    string( REGEX MATCH "([0-9]+)\.[0-9]+\.[0-9]+(\.[0-9]+)?" TMP ${versionString} )
+    if( CMAKE_MATCH_COUNT EQUAL 0 )
+        message( FATAL_ERROR "    Could not determine major version from '${versionString}'" )
+    endif()
+    
+    SET( ${VARNAME} ${CMAKE_MATCH_1} PARENT_SCOPE )
+endfunction()
+
+FUNCTION( getDisplayNameYear displayName VARNAME)
+    string( REGEX MATCH "[A-Za-z ]+([0-9][0-9][0-9][0-9])" TMP ${displayName} )
+    if( CMAKE_MATCH_COUNT EQUAL 0 )
+        message( FATAL_ERROR "    Could not determine year from the displayName '${displayName}'" )
+    endif()
+    
+    SET( ${VARNAME} ${CMAKE_MATCH_1} PARENT_SCOPE )
+endfunction()
+
 FUNCTION(VerifyVisualStudio PREFIX)
     SET( ${PREFIX}VISUAL_STUDIO_FOUND FALSE PARENT_SCOPE )
     IF( NOT WIN32)
@@ -53,92 +72,100 @@ FUNCTION(VerifyVisualStudio PREFIX)
     #message( STATUS "VSARCH=${VSARCH}" )
     
     execute_process( 
-        COMMAND ${VSWHERE} -requires Microsoft.VisualStudio.Component.VC.Tools.x86.x64 -property installationPath
-        OUTPUT_VARIABLE installPaths
+        COMMAND ${VSWHERE} -requires Microsoft.VisualStudio.Component.VC.Tools.x86.x64 -format JSON
+        OUTPUT_VARIABLE fullJSON
         ERROR_QUIET
     )
-    string( STRIP ${installPaths} installPaths )
-    STRING( REPLACE "\n" ";" installPaths ${installPaths} )
-
-    execute_process( 
-        COMMAND ${VSWHERE} -requires Microsoft.VisualStudio.Component.VC.Tools.x86.x64 -property productPath
-        OUTPUT_VARIABLE devEnvPaths
-        ERROR_QUIET
-    )
-    string( STRIP ${devEnvPaths} devEnvPaths )
-    STRING( REPLACE "\n" ";" devEnvPaths ${devEnvPaths} )
-
-    execute_process( 
-        COMMAND ${VSWHERE} -requires Microsoft.VisualStudio.Component.VC.Tools.x86.x64 -property catalog_productLineVersion
-        OUTPUT_VARIABLE installProductLineVersions
-        ERROR_QUIET
-    )
-    string( STRIP ${installProductLineVersions} installProductLineVersions )
-    STRING( REPLACE "\n" ";" installProductLineVersions ${installProductLineVersions} )
-
-    execute_process( 
-        COMMAND ${VSWHERE} -requires Microsoft.VisualStudio.Component.VC.Tools.x86.x64 -property displayName
-        OUTPUT_VARIABLE installDisplayNames
-        ERROR_QUIET
-    )
-    string( STRIP ${installDisplayNames} installDisplayNames )
-    STRING( REPLACE "\n" ";" installDisplayNamesTmp ${installDisplayNames} )
-    SET( installDisplayNames "" )
-    foreach( displayName ${installDisplayNamesTmp} )
-        string( REGEX MATCH "[A-Za-z ]+([0-9][0-9][0-9][0-9])" TMP ${displayName} )
-        if( CMAKE_MATCH_COUNT EQUAL 0 )
-            message( FATAL_ERROR "    Could not determine year from the installDisplayName '${displayName}'" )
-        endif()
-        
-        LIST( APPEND installDisplayNames ${CMAKE_MATCH_1} )
-    endforeach()
-
-    execute_process( 
-        COMMAND ${VSWHERE} -requires Microsoft.VisualStudio.Component.VC.Tools.x86.x64 -property installationVersion
-        OUTPUT_VARIABLE installVersions
-        ERROR_QUIET
-    )
-    string( STRIP ${installVersions} installVersions )
-    STRING( REPLACE "\n" ";" installVersionsTmp ${installVersions} )
+    string( JSON num LENGTH ${fullJSON} )
+    math( EXPR num "${num}-1")
     SET( installVersions "" )
-    #message( STATUS " installVersionsTmp=${installVersionsTmp}" )
-    foreach( installVersion ${installVersionsTmp} )
-        string( REGEX MATCH "([0-9]+)\.[0-9]+\.[0-9]+(\.[0-9]+)?" TMP ${installVersion} )
-        if( CMAKE_MATCH_COUNT EQUAL 0 )
-            message( FATAL_ERROR "    Could not determine installVersion's version from '${installVersion}'" )
-        endif()
+    SET( installPaths "" )
+    SET( productPaths "" )
+    SET( displayNames "")
+    SET( productLineVersions "" )
+    SET( featureReleaseYears "" )
+    
+    
+    foreach( currInstallNum RANGE 0 ${num} )
+        string( JSON currInstallInfo ERROR_VARIABLE errorVar GET ${fullJSON} ${currInstallNum} )
+        #message( STATUS "errorVar=${errorVar}" )
+        #message( STATUS currInstallInfo=${currInstallInfo} )
         
-        LIST( APPEND installVersions ${CMAKE_MATCH_1} )
+        STRING( JSON installVersion ERROR_VARIABLE errorVar GET ${currInstallInfo} "installationVersion" )
+        #message( STATUS "installVersion=${installVersion}" )
+        #message( STATUS "errorVar=${errorVar}" )
+        list( APPEND installVersions ${installVersion} )
+        
+        STRING( JSON installationPath ERROR_VARIABLE errorVar GET ${currInstallInfo} "installationPath" )
+        #message( STATUS "errorVar=${errorVar}" )
+        #message( STATUS "installationPath=${installationPath}" )
+        list( APPEND installPaths ${installationPath} )
+        
+        STRING( JSON productPath ERROR_VARIABLE errorVar GET ${currInstallInfo} "productPath" )
+        #message( STATUS "errorVar=${errorVar}" )
+        #message( STATUS "productPath=${productPath}" )
+        list( APPEND productPaths ${productPath} )
+
+        STRING( JSON displayName ERROR_VARIABLE errorVar GET ${currInstallInfo} "displayName" )
+        #message( STATUS "errorVar=${errorVar}" )
+        list( APPEND displayNames ${displayName} )
+
+        string( JSON currCatalog GET ${currInstallInfo} "catalog" )
+        #message( STATUS "errorVar=${errorVar}" )
+        #message( STATUS "currCatalog=${currCatalog}" )
+
+        STRING( JSON featureReleaseYear ERROR_VARIABLE errorVar GET ${currCatalog} "featureReleaseYear" )
+        #message( STATUS "errorVar=${errorVar}" )
+        #message( STATUS "featureReleaseYear=${featureReleaseYear}" )
+        list( APPEND featureReleaseYears ${featureReleaseYear} )
+
+        STRING( JSON productLineVersion ERROR_VARIABLE errorVar GET ${currCatalog} "productLineVersion" )
+        #message( STATUS "errorVar=${errorVar}" )
+        #message( STATUS "productLineVersion=${productLineVersion}" )
+        list( APPEND productLineVersions ${productLineVersion} )
     endforeach()
 
     #message( STATUS "installPaths=${installPaths}" )
-    #message( STATUS "devEnvPaths=${devEnvPaths}" )
-    #message( STATUS "installProductLineVersions=${installProductLineVersions}" )
-    #message( STATUS "installVersions=${installVersions}" )
-   
+    #message( STATUS "productPaths=${productPaths}" )
+    #message( STATUS "displayNames=${displayNames}" )
+    #message( STATUS "productLineVersions=${productLineVersions}" )
+    #message( STATUS "featureReleaseYears=${featureReleaseYears}" )
     
-    foreach( installPath devEnvPath installVersion displayName IN ZIP_LISTS installPaths devEnvPaths installVersions installDisplayNames )
+    foreach( installPath productPath displayName installVersion productLineVersion featureReleaseYear IN ZIP_LISTS installPaths productPaths displayNames installVersions productLineVersions featureReleaseYears )
         file(TO_CMAKE_PATH ${installPath} installPath)
-        file(TO_CMAKE_PATH ${devEnvPath} devEnvPath)
+        file(TO_CMAKE_PATH ${productPath} productPath)
 
+        #message( STATUS "===========================" )
         #message( STATUS "installPath=${installPath}" )
-        #message( STATUS "installVersion=${installVersion}" )
+        #message( STATUS "productPath=${productPath}" )
+        getDisplayNameYear( ${displayName} displayNameYear )
         #message( STATUS "displayName=${displayName}" )
-        #message( STATUS "devEnvPath=${devEnvPath}" )
+        #message( STATUS "displayNameYear=${displayNameYear}" )
 
-        if ( ${devEnvPath} STREQUAL ${DEVENV} )
-            #MESSAGE( STATUS "Found DevEnv in use: ${DEVENV} - ${devEnvPath}" )
+        #message( STATUS "installVersion=${installVersion}" )
+        getInstallMajorVersion( ${installVersion} majorVersion )
+        #message( STATUS "majorVersion=${majorVersion}" )
+        #message( STATUS "productLineVersion=${productLineVersion}" )
+        #message( STATUS "featureReleaseYear=${featureReleaseYear}" )
+
+        if ( ${productPath} STREQUAL ${DEVENV} )
+            #MESSAGE( STATUS "Found DevEnv in use: ${DEVENV} - ${productPath}" )
             
             SET( ${PREFIX}VISUAL_STUDIO_FOUND TRUE PARENT_SCOPE )
             SET( ${PREFIX}VISUAL_STUDIO_INSTALLPATH ${installPath} PARENT_SCOPE )
-            SET( ${PREFIX}VISUAL_STUDIO_PRODUCTLINEVERSION ${displayName} PARENT_SCOPE )
+            SET( ${PREFIX}VISUAL_STUDIO_PRODUCTLINEVERSION ${productLineVersion} PARENT_SCOPE )
             SET( ${PREFIX}VISUAL_STUDIO_INSTALLVERSION ${installVersion} PARENT_SCOPE )
             
-            SET( generator "Visual Studio ${installVersion} ${displayName}" )
-            if ( ( ${productLineVersion} LESS 2019 ) AND ( ${VSARCH} STREQUAL "x64" ) )
-                SET( generator "${generator} Win64" )
-            ENDIF()
-                    
+            SET( generator "Visual Studio ${majorVersion}" )
+            if ( featureReleaseYear )
+                SET( generator "${generator} ${featureReleaseYear}" )
+            elseif ( productLineVersion )
+                SET( generator "${generator} ${productLineVersion}" )
+            elseif ( displayNameYear )
+                SET( generator "${generator} ${displayNameYear}" )
+            endif()
+            
+            #message( STATUS "generator = ${generator}" )
             SET( ${PREFIX}VISUAL_STUDIO_GENERATOR ${generator} PARENT_SCOPE )
             if ( NOT "${generator}" STREQUAL "${CMAKE_GENERATOR}" )
                 MESSAGE( FATAL_ERROR "    Visual Studio should use generator '${generator}' is not the same as the current CMAKE_GENERATOR '${CMAKE_GENERATOR}'\n    Please use -G \"${generator}\" or set the environment variable CMAKE_GENERATOR=\"${generator}\"" )
